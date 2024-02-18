6c329057364d7464e38507f37522c266cd09ccbb
Yilin Yang <yyl_20050115@hotmail.com>
2024-02-18 10:09:56 +0800

    添加项目文件。

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿namespace VSRemove;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        if (args.Length == 0 || args.Length > 0 && !Directory.Exists(args[0]))
+        {
+            Console.WriteLine("VSRemove <Path> [dirname:.vs]");
+            Console.WriteLine("   Removes all .vs/other folders within speicified path to free disk space.");
+        }
+        else
+        {
+            var path = args[0];
+            if (Directory.Exists(path))
+            {
+                var ext = args.Length > 1 ? args[1] : ".vs";
+                Console.WriteLine("Searching for directories...");
+                var directories = Directory.GetDirectories(path, ext, SearchOption.AllDirectories);
+                foreach(var d in directories)
+                {
+                    Console.WriteLine(d);
+                    try
+                    {
+                        Directory.Delete(d, true);
+                    }catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.ToString());
+                    }
+                }
+            }
+        }
+    }
+}
